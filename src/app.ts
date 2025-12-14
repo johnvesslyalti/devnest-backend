583059de48fed84789512ac1ac610bcf25a80a2e
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-14 13:56:08 +0530

    feat: add the follow and unfollow

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import postRoutes from "./modules/post/post.routes"
 import likeRoutes from "./modules/like/like.routes"
 import commentRoutes from "./modules/comment/comment.routes"
 import homeRoutes from "./modules/home/home.routes"
+import followRoutes from "./modules/follow/follow.routes"
 
 const app = express()
 app.use(express.json())
@@ -13,6 +14,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/likes", auth.verifyAccessToken, likeRoutes);
 app.use("/api/v1/comments", auth.verifyAccessToken, commentRoutes);
+app.use("/api/v1/follow", auth.verifyAccessToken, followRoutes)
 app.use("/api/v1/home", homeRoutes)
 
 app.get("/health", (_, res) => {
